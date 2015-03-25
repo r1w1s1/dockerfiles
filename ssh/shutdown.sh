98b8e1a3f29ea91f527f12fe1c658b351acf849a
ricardsonwilliams <ricardsonwilliams@Macintosh.local>
2015-03-25 09:11:04 -0300

    updates...

--- ssh/shutdown.sh
+++
@@ -1,3 +0,0 @@
-docker stop test_sshd
-docker rm test_sshd
-docker rmi eg_sshd
--- ssh/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-docker build -t eg_sshd .
-docker run -d -P --name test_sshd eg_sshd
-docker port test_sshd 22
