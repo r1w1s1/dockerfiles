5a78ec0e121c16f2e953a6d14b71cb0ae1a661c2
r1w1s1 <r1w1s1@icloud.com>
2023-08-16 20:37:58 -0300

    apache+php container

---
+++ httpd-php80/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Apache with PHP Support - Almalinux 9</title>
+<style>
+    body {
+        text-align: center;
+        font-family: Tahoma, Geneva, Verdana, sans-serif;
+    }
+</style>
+</head>
+<body>
+<h1>Apache with PHP Support.</h1>
+<p>If you see PHP info below, Apache with PHP container works.</p>
+
+<?php 
+
+   phpinfo()
+   
+?>
+</body>
+</html>
